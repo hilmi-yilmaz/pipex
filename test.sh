b7584b4e38c616512b859d7eb71e807d8bdeaa02
Hilmi Yilmaz <hilmiyilmaz.1998@gmail.com>
2021-11-02 17:31:01 +0100

    restructured tester

--- test.sh
+++ test.sh
@@ -7,35 +7,68 @@ file_in="/tmp/file_in"
 
 # Create input file
 cat << EOF > $file_in
-These contents will be written to the file.
-        This line is indented.
+codamcodingcollege
+codam
+ahhaha
+codamamsterdam
+codanmgotcha
 EOF
 
-# Run bash
-< file_in grep hilmi | wc -l > file_out_bash
-exit_status_bash=$(echo "$?")
+# $1: input file
+# $2: cmd1
+# $3: cmd2
+# $4: output file
+run_pipex()
+{
+	./pipex "$1" "$2" "$3" "$4"
+	echo "$?"
+}
 
-# Run yours
-./pipex file_in "grep hilmi" "wc -l" file_out_yours
-exit_status_yours=$(echo "$?")
+# $1 whole command to execute
+run_bash()
+{
+	bash -c "$1"
+	echo "$?"
+}
 
-# Check whether file_out exists
-if [[ ! -f file_out_yours ]]; then
-	echo "Error: file_out not created."
-fi
+# $1: exit_status_bash
+# $2: exit_status_yours 
+compare_outputs()
+{
 
-# Compare outputs
-if [[ "$exit_status_bash" == "$exit_status_yours" ]]; then
-	echo "SUCCES: Exit codes correct."
-else
-	echo "ERROR: Exit codes differ."
-fi
+	cat file_out_yours
+	echo ""
+	cat file_out_bash
+	# Check whether file_out exists
+	if [[ ! -f file_out_yours ]]; then
+		echo "Error: file_out not created by pipex."
+	fi
 
-diff file_out_bash file_out_yours >> /dev/null
-if [[ "$?" == "0" ]]; then
-	echo "SUCCES: Output files the same."
-else
-	echo "ERROR: Output files differ."
-fi
+	# Compare exit status
+	if [[ "$1" == "$2" ]]; then
+		echo "SUCCES: Exit codes correct."
+	else
+		echo "ERROR: Exit codes differ."
+	fi
 
-rm -f file_out_bash file_out_yours
+	# Compare output files
+	diff file_out_bash file_out_yours > /dev/null
+	if [[ "$?" == "0" ]]; then
+		echo "SUCCES: Output files the same."
+	else
+		echo "ERROR: Output files differ."
+	fi
+
+	# Delete created files
+	rm -f file_out_bash file_out_yours
+}
+
+# Test 1
+exit_status_bash=$(run_bash "< $file_in grep contents | wc -l > file_out_bash")
+exit_status_yours=$(run_pipex "$file_in" "grep contents" "wc -l" "file_out_yours")
+compare_outputs "$exit_status_bash" "$exit_status_yours"
+
+# Test 2
+exit_status_bash=$(run_bash "< $file_in ls -la | wc -w > file_out_bash")
+exit_status_yours=$(run_pipex "$file_in" "ls -la" "wc -w" file_out_yours)
+compare_outputs "$exit_status_bash" "$exit_status_yours"
